0c33b3f9122d2bc65a9b190c914a62c275ba9cb2
Simon Pinfold <simon.pinfold@imgtec.com>
2015-02-18 09:18:41 +1300

    Cleaned up tozip script

--- tozip.sh
+++ tozip.sh
@@ -1,1 +1,1 @@
-git archive -v -o WiFire.zip HEAD $(git diff --name-only `git rev-list --parents HEAD | egrep "^[a-f0-9]{40}$"` HEAD | grep -v tozip.sh) 2>&1 | less +G -X -E
+git archive -v -o WiFire_Flow.zip HEAD $(git diff --name-only `git rev-list --parents HEAD | egrep "^[a-f0-9]{40}$"` HEAD | grep -v tozip.sh)
